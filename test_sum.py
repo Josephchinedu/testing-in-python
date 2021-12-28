6d39d2e0bed469da3d0ffeebbb72e43be9ed229f
Joseph Chinedu <josephchinedu34@gmail.com>
2021-12-28 15:39:07 +0100

    test code basics

---
+++ test_sum.py
@@ -0,0 +1,16 @@
+# def test_sum():
+#     assert sum([1,2,3]) == 6
+
+# if __name__ == "__main__":
+#     test_sum()
+#     print("everything works fine")
+
+def test_sum():
+    assert sum([1, 2, 3]) == 6, "answer should be 6"
+        
+def test_sum_tuple():
+    assert sum((1, 2, 2)) == 7, "answer should be 5"
+
+
+   
+    
